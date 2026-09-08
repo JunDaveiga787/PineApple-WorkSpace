ff50ca1e2079260b7d0b235066135f89a127ed23
Jun Daveiga <98670262+JunDaveiga787@users.noreply.github.com>
2026-09-08 02:39:55 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pineapple-app-v14';
+const CACHE_NAME = 'pineapple-app-v15';
 const APP_FILES = ['./', './index.html', './style.css', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
